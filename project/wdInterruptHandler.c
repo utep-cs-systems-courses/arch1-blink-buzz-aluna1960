a7a03317137760fcc000480b858c208b7e2093b8
Adrian Luna <aaluna5@miners.utep.edu>
2021-03-26 17:55:34 -0600

    Upload for lab 2, my Makefile is not working, or at least not executing correctly since it is not executing any of the operations on the msp430.

---
+++ project/wdInterruptHandler.c
@@ -0,0 +1,17 @@
+#include <msp430.h>
+#include "stateMachines.h"
+
+void
+__interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
+  static char buzz_count = 0;
+  static char blink_count = 0;
+  if (++blink_count == 125) {
+    state_advance();
+    blink_count = 0;
+  }
+
+  if (++buzz_count == 120){
+    noise();
+    buzz_count = 0;
+  }
+}
